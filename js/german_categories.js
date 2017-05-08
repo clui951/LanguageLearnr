3243f35ffaaa0134ebdd3ad684b9bbc020599c26
Michael Deng <michaeldeng18@gmail.com>
2017-05-08 16:46:50 -0700

    Reorder JS

--- js/german_categories.js
+++ js/german_categories.js
@@ -40,8 +40,6 @@ for (var i = 0; i < resList.length; i++) {
 	categoriesList[categoryName] = id;
 }
 
-// document.getElementById("checkbox").checked = true;
-
 var x = document.getElementsByClassName("category-box");
 
 var travel = document.getElementById("travel-checkbox");
@@ -58,4 +56,35 @@ if (sports.nextSibling.textContent.trim() in categoriesList) {
 
 if (fashion.nextSibling.textContent.trim() in categoriesList) {
 	fashion.checked = true;
+}
+
+function handleRequest() {
+  if (travel.checked && !("Travel" in categoriesList)) {
+  	var data = {};
+  	data["fields"] = {};
+  	data["fields"]["Name"] = "Travel";
+  	httpPost("https://api.airtable.com/v0/appzAhnNiUb6ky3nW/Table%201", JSON.stringify(data));
+  } else if (!travel.checked && "Travel" in categoriesList) {
+  	httpDelete("https://api.airtable.com/v0/appzAhnNiUb6ky3nW/Table%201/" + categoriesList["Travel"]);
+  }
+
+  if (sports.checked && !("Sports" in categoriesList)) {
+  	var data = {};
+  	data["fields"] = {};
+  	data["fields"]["Name"] = "Sports";
+  	httpPost("https://api.airtable.com/v0/appzAhnNiUb6ky3nW/Table%201", JSON.stringify(data));
+  } else if (!sports.checked && "Sports" in categoriesList) {
+  	httpDelete("https://api.airtable.com/v0/appzAhnNiUb6ky3nW/Table%201/" + categoriesList["Sports"]);
+  }
+
+  if (fashion.checked && !("Fashion" in categoriesList)) {
+  	var data = {};
+  	data["fields"] = {};
+  	data["fields"]["Name"] = "Fashion";
+  	httpPost("https://api.airtable.com/v0/appzAhnNiUb6ky3nW/Table%201", JSON.stringify(data));
+  } else if (!fashion.checked && "Fashion" in categoriesList) {
+  	httpDelete("https://api.airtable.com/v0/appzAhnNiUb6ky3nW/Table%201/" + categoriesList["Fashion"]);
+  }
+
+  return false;
 }
